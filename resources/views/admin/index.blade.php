ba709d6f2b504e47d8df0c730341b82caeb5bca0
Lprabodha <lprabodha1998@gmail.com>
2025-06-07 01:26:49 +0530

    Change Dashboard view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,13 +3,15 @@
 @section('content')
     <div class="dashboard-main-body">
         <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
-            <h6 class="fw-semibold mb-0">Dashboard</h6>
+
+            <h6 class="fw-semibold mb-0">Welcome to Click2 Clickin</h6>
 
             @if (!auth()->user()->hasRole('hotel-clerk'))
                 <ul class="d-flex align-items-center gap-2">
                     <li class="fw-medium">
                         <a href="index.html" class="d-flex align-items-center gap-1 hover-text-primary">
                             <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
+
                             Dashboard
                         </a>
                     </li>
@@ -20,7 +22,8 @@
 
             @if (auth()->user()->hasRole('hotel-clerk'))
                 <div class="ms-auto m-2">
-                    <a href="{{route('admin.reservation.create')}}" class="btn btn-primary d-flex align-items-center gap-2">
+                    <a href="{{ route('admin.reservation.create') }}"
+                        class="btn btn-primary d-flex align-items-center gap-2">
                         <i class="bi bi-plus-lg"></i> + New Reservation
                     </a>
                 </div>
