147eae4557275d35d32f53237f75107526f2cb84
Lprabodha <lprabodha1998@gmail.com>
2025-06-08 00:12:28 +0530

    Fixed Cronjob issue

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-$schedule->command('reservation:auto-cancel')->dailyAt('19:00'); // 7 PM
-$schedule->command('billing:no-show')->dailyAt('19:00'); // 7 PM
+Schedule::command('reservation:auto-cancel')->dailyAt('19:00'); // 7 PM
+Schedule::command('billing:no-show')->dailyAt('19:00'); // 7 PM
